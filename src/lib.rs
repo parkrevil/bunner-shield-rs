82232b208fa3d9cc191682b7f97a3feceef00b69
revil <revil.com@gmail.com>
2025-10-08 23:31:06 +0900

    feat: add Shield structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
+mod shield;
 
+pub use crate::shield::Shield;
---
+++ src/shield.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct Shield;
+
+impl Shield {
+    pub fn new() -> Self {
+        Self
+    }
+}
