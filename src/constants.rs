f0c6758a38602429479750475ba387dcae282f2e
revil <revil.com@gmail.com>
2025-10-09 03:04:19 +0900

    chore: remove deadcode

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,3 @@
-#[allow(dead_code)]
 pub mod header {
     pub const CONTENT_SECURITY_POLICY: &str = "Content-Security-Policy";
     pub const CONTENT_SECURITY_POLICY_REPORT_ONLY: &str = "Content-Security-Policy-Report-Only";
